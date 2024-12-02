5eed6d0b2ce3f87aef2fe4eeb953220a0a668f4b
Ethan Crawford <129777161+ecrawford4@users.noreply.github.com>
2024-12-02 12:05:35 -0800

    refactor js

--- script.js
+++ script.js
@@ -3,14 +3,14 @@ document.addEventListener('DOMContentLoaded', () => {
   const dateTimeString = new Date().toLocaleString();
   const [datePart, timePart] = dateTimeString.split(', ');
   document.getElementById('current-session').innerHTML = `Current Session:<br>${datePart}<br>${timePart}`;
-  
+
   // real time clock display: update the time every second
   setInterval(() => {
     const dateTimeString = new Date().toLocaleString();
     const [datePart, timePart] = dateTimeString.split(', ');
     document.getElementById('real-time-clock').innerHTML = `Current Time:<br>${datePart}<br>${timePart}`;
   }, 1000);
-  
+
   // fetch the data from the data.json file
   const dataSource = "https://ecrawford4-assets.s3.us-east-2.amazonaws.com/data.json";
 
@@ -24,24 +24,28 @@ document.addEventListener('DOMContentLoaded', () => {
     .then(data => {
       console.log('Data fetched successfully:', data);
 
+      // select the project container
       const projectContainer = document.querySelector("figure.pinned-project-grid-container");
       if (!projectContainer) {
         console.error('Project container not found');
         return;
       }
 
+      //  populate pinned projects
       data.pinnedProjects.forEach(project => {
         const projectTemplate = `
         <a class="grid-item" target="_blank" href="${project.link}">${project.title}</a>`;
         projectContainer.insertAdjacentHTML('beforeend', projectTemplate);
       })
 
+      // select the contact container
       const contactContainer = document.querySelector("div.contact");
       if (!contactContainer) {
         console.error('Contact container not found');
         return;
       }
 
+      // populate social badges
       data.socials.forEach(social => {
         const socialTemplate = `
         <a class="social-badge" target="_blank" href="${social.socialLink}">
@@ -49,6 +53,25 @@ document.addEventListener('DOMContentLoaded', () => {
         </a>
       `;
         contactContainer.insertAdjacentHTML('beforeend', socialTemplate);
+      });
+
+      // select the music container
+      const container = document.querySelector("figure.music-folio-grid-container");
+      if (!container) {
+        console.error('figure.music-folio-grid-container container not found');
+        return;
+      }
+
+      // populate music portfolio
+      data.music.forEach(key => {
+        const template = `
+          <figure>
+            <h2 class="music-folio">${key.title}</h2>
+            <audio src="${key.audioSrc}" controls></audio>
+            <a class="music-folio-badge" target="_blank" href="${key.scoreSrc}">View Score</a>
+          </figure>
+        `;
+        container.insertAdjacentHTML('afterbegin', template);
       });
     });
 });
